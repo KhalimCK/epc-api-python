6ebf6d7a30145197d13a5ac7c94f4e82b07f6ba3
Khalim Conn-Kowlessar <khalim@exii.co>
2023-04-12 18:46:42 +0100

    fixed init of client and added test

--- tests/test_client_init.py
+++ tests/test_client_init.py
@@ -5,32 +5,48 @@ import base64
 
 
 def test_failed_init_all_args_null():
+    # No credentials supplied, should error
     with pytest.raises(MissingAuth) as _:
-        EpcClient(None, None)
+        EpcClient()
 
 
-def test_failed_init_auth_token_null():
+def test_successful_init_auth_token():
 
-    test_auth_token = base64.b64encode(b"test")
+    # Auth token supplied, should be successful
+    test_auth_token = base64.b64encode(b"test@user.com:<api_key>")
 
-    with pytest.raises(MissingAuth) as _:
-        EpcClient(test_auth_token, None)
+    client = EpcClient(auth_token=test_auth_token)
+    assert client.auth_token == test_auth_token
+
+    assert client.api_key is None
+    assert client.user_email is None
 
 
-def test_failed_init_user_email_null():
-
+def test_failed_init_just_email():
+    # Just user email supplied, needs api key
     test_user_email = "test@user.com"
 
     with pytest.raises(MissingAuth) as _:
-        EpcClient(None, test_user_email)
+        EpcClient(user_email=test_user_email)
+
+
+def test_failed_init_just_api_key():
+    # Just user email supplied, needs api key
+    test_api_key = "testapikey"
+
+    with pytest.raises(MissingAuth) as _:
+        EpcClient(api_key=test_api_key)
 
 
 def test_successful_init():
 
     test_user_email = "test@user.com"
-    test_auth_token = base64.b64encode(b"test")
+    test_api_key = "testapikey"
+    expected_auth_token = base64.b64encode(":".join([test_user_email, test_api_key]).encode("utf-8"))
 
-    client = EpcClient(test_auth_token, test_user_email)
+    client = EpcClient(user_email=test_user_email, api_key=test_api_key)
 
     assert client.auth_token is not None
     assert client.user_email is not None
+    assert client.api_key is not None
+    assert client.auth_token == expected_auth_token
