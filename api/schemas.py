21170dc8d438338d4ffc6adcde76b16c69493588
Khalim Conn-Kowlessar <khalim@exii.co>
2023-04-14 10:50:10 +0100

    initial setup of params schema

--- api/schemas.py
+++ api/schemas.py
@@ -1,47 +1,45 @@
 import pydantic
+from enum import Enum
+from typing import Literal, Optional
+
+
+class PropertyType(Enum):
+    bungalow: str
+    flat: str
+    house: str
+    maisonette: str
+    park_home: str = pydantic.Field(alias="park home")
+
+
+class FloorArea(Enum):
+    unknown: str
+    s: str
+    m: str
+    l: str
+    xl: str
+    xxl: str
+    xxxl: str
+
+
+class EnergyBand(Enum):
+    a: str
+    b: str
+    c: str
+    d: str
+    e: str
+    f: str
+    g: str
 
 
 class ParamSchema(pydantic.BaseModel):
-    address: str
-    postcode: str
-    local_authority: str = pydantic.Field(alias="local-authority")
-
-# params:
-    # - address
-    # - postcode
-    # - local-authority
-    # - constituency
-    # - property-type Multiple property types can be supplied so this should be a list if supplied
-    # In this initial implementation, we won't do much parameter validation and allow the api failure to handle that
-    # We will validate proprty-type
-    # bungalow	Filter for properties of type 'Bungalow'
-    # flat	Filter for properties of type 'Flat'
-    # house	Filter for properties of type 'House'
-    # maisonette	Filter for properties of type 'Maisonette'
-    # park home	Filter for properties of type 'Park Home'
-    # - floor-area Again multiple can be supplied
-    # unknown	A floor area recorded as 0
-    # s	A floor area between 1m² and 55m²
-    # m	A floor area between 55m² and 70m²
-    # l	A floor area between 70m² and 85m²
-    # xl	A floor area between 85m² and 110m²
-    # xxl	A floor area greater than 110m²
-    # xxxl
-    # - energy-band
-    # a	A rated (92+)
-    # b	B rated (81-91)
-    # c	C rated (69-80)
-    # d	D rated (55-68)
-    # e	E rated (39-54)
-    # f	F rated (21-38)
-    # g	G rated (1-20)
-    # - from-month	A numeric month identifier 1-12, to establish the start of a date range, where 1 is January and 12 is December. If no from-month parameter is supplied 1 (January) is assumed.
-    # - from-year	A numeric year identifier to estalish the start of a date range between 2008 and 2023 e.g. 2015. If no year parameter is supplied 2008 is assumed.
-    # - to-month	A numeric month identifier 1-12, to establish the end of a date range, where 1 is January and 12 is December. If no to-month parameter is supplied then 12 is assumed.
-    # - to-year	A numeric year identifier between 2008 and 2023 e.g. 2015. If no to-year parameter is supplied then 2023 is assumed.
-params_schema = Schema(
-    {
-        Optional("address"): str,
-
-    }
-)
+    address: Optional[str]
+    postcode: Optional[str]
+    local_authority: Optional[str] = pydantic.Field(alias="local-authority")
+    constituency: Optional[str]
+    property_type: Optional[PropertyType] = pydantic.Field(alias="property-type")
+    floor_area: Optional[FloorArea] = pydantic.Field(alias="floor-area")
+    energy_band: Optional[EnergyBand] = pydantic.Field(alias="energy-band")
+    from_month: Optional[int] = pydantic.Field(None, ge=1, le=12, alias="from-month")
+    from_year: Optional[int] = pydantic.Field(None, ge=2008, alias="from-year")
+    to_month: Optional[int] = pydantic.Field(None, ge=1, le=12, alias="to-month")
+    to_year: Optional[int] = pydantic.Field(None, ge=2008, alias="to-year")
