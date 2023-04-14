4df002c2ca98e547ec46ddad492eb82a3e2c54d8
Khalim Conn-Kowlessar <khalim@exii.co>
2023-04-14 17:20:51 +0100

    testing out the other endpoints

--- examples/test_example.py
+++ examples/test_example.py
@@ -7,7 +7,12 @@ dotenv.load_dotenv()
 client = EpcClient(version="v1")
 
 search_resp = client.domestic.search(params={})
+print(search_resp)
 
-# Test csv accept response
-client = EpcClient(version="v1", accept="application/vnd.ms-excel")
-search_resp = client.domestic.search(params={})
+# Test the certificate endpoint
+certificate_resp = client.domestic.certificate(lmk_key=search_resp["rows"][0]["lmk-key"])
+print(certificate_resp)
+
+# Test the recommendations endpoint
+recommendation_resp = client.domestic.recommendations(lmk_key=search_resp["rows"][0]["lmk-key"])
+print(recommendation_resp)
